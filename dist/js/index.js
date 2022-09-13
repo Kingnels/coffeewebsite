3c07ce204d5ce26bc65bfb73e9d15db2463c11fd
Kingnels <ifeanyiibe2022@gmail.com>
2022-09-13 17:34:29 +0100

    work in progress

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,1 +1,32 @@
-let navbar = document.querySelector(".navbar")
+let navbar = document.querySelector(".navbar");
+document.querySelector('#menu-btn').onclick = ()=> {
+    navbar.classList.toggle("inactive");
+}
+
+
+let searchForm = document.querySelector(".search-form");
+document.querySelector('#search-btn').onclick = ()=> {
+    searchForm.classList.toggle("inactive");
+}
+
+
+let cartItem = document.querySelector(".cart-items-container");
+document.querySelector('#cart-btn').onclick = ()=> {
+    cartItem.classList.toggle("inactive");
+}
+
+
+
+
+
+
+
+
+
+
+// alert("hi")
+
+
+
+
+
